2f6aeda7e6c2de332c918dd64cb678879554319f
Cecil Montemayor <cecilmontemayoriv@gmail.com>
2020-11-18 12:44:41 -0500

    made code more readable

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 //occam's razor
 
+const chalk = require("chalk");
+const boxen = require("boxen");
 const fs = require('fs');
 const fetch = require('node-fetch')
 const pokemonNames = process.argv[2];
 
 var pokeContent = (fs.readFileSync(pokemonNames).toString()).split("\n");
-pokeContent = pokeContent.map(i => i.toLowerCase());
+pokeContent = pokeContent.map(pokemonName => pokemonName.toLowerCase());
 
  let atributes = (pokeContent) => {
      pokeContent.forEach(element => {
@@ -17,11 +19,11 @@ pokeContent = pokeContent.map(i => i.toLowerCase());
                     let pokeTypes = data.types;
                     name = name[0].toUpperCase() + name.substr(1,name.length).toLowerCase();
                     let output = name + ":";
-                    pokeTypes.forEach(j => {
-                        if (j.slot == 1) {
-                            output = output + " " + j.type.name
+                    pokeTypes.forEach(typeObject => {
+                        if (typeObject.slot == 1) {
+                            output = output + " " + typeObject.type.name
                         } else {
-                            output = output + ", " + j.type.name
+                            output = output + ", " + typeObject.type.name
                         }
                     });
                     console.log(output);
@@ -32,8 +34,7 @@ pokeContent = pokeContent.map(i => i.toLowerCase());
 atributes(pokeContent);
 
 
-// const chalk = require("chalk");
-// const boxen = require("boxen");
+
 
 // const greeting = chalk.white.bold("Hello!");
 
@@ -44,6 +45,6 @@ atributes(pokeContent);
 //  borderColor: "green",
 //  backgroundColor: "#555555"
 // };
-// const msgBox = boxen( greeting, boxenOptions );
+// const msgBox = boxen( atributes(pokeContent), boxenOptions );
 
 // console.log(msgBox);
